8b2ea184ca5999cfdce218fa7309b862d9083088
Daniel Wilmer <d.wilmer.1980@googlemail.com>
2015-08-06 02:24:51 +0200

    Update default colorscheme

--- highlighter.go
+++ highlighter.go
@@ -12,20 +12,20 @@ var resetColor = "\x1b[0m"
 type Colors map[jsont.TokenType]string
 
 var DefaultColors = Colors{
-	jsont.ObjectStart: "\x1b[90m",
-	jsont.ObjectEnd:   "\x1b[90m",
-	jsont.ArrayStart:  "\x1b[90m",
-	jsont.ArrayEnd:    "\x1b[90m",
-	jsont.Colon:       "\x1b[90m",
-	jsont.Comma:       "\x1b[90m",
+	jsont.ObjectStart: "\x1b[38;5;242m",
+	jsont.ObjectEnd:   "\x1b[38;5;242m",
+	jsont.ArrayStart:  "\x1b[38;5;242m",
+	jsont.ArrayEnd:    "\x1b[38;5;242m",
+	jsont.Colon:       "\x1b[38;5;242m",
+	jsont.Comma:       "\x1b[38;5;242m",
 	jsont.FieldName:   "\x1b[38;5;33m",
-	jsont.True:        "\x1b[36m",
-	jsont.False:       "\x1b[36m",
-	jsont.Null:        "\x1b[36m",
-	jsont.Integer:     "\x1b[36m",
-	jsont.Float:       "\x1b[36m",
-	jsont.String:      "\x1b[36m",
-	jsont.Unknown:     "\x1b[31m",
+	jsont.True:        "\x1b[38;5;22m",
+	jsont.False:       "\x1b[38;5;124m",
+	jsont.Null:        "\x1b[38;5;124m",
+	jsont.Integer:     "\x1b[38;5;117m",
+	jsont.Float:       "\x1b[38;5;117m",
+	jsont.String:      "\x1b[38;5;45m",
+	jsont.Unknown:     "\x1b[38;5;1m",
 }
 
 func HighlightString(jsonString string) (string, error) {
